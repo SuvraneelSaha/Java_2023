19194ab423b826eefe993be6d983dfbdebbc3886
Suvraneel _Saha <81958235+SuvraneelSaha@users.noreply.github.com>
2023-02-21 10:23:35 +0530

    Login

---
+++ Self Learning Java/src/Day1/O1.java
@@ -0,0 +1,15 @@
+package Day1;
+
+public class O1 {
+    public static void main(String[] args) {
+        System.out.println("Yo Neo!!" +
+                "you know ");
+        System.out.println("this is your last chance");
+        System.out.println("Choose the Red pill and i offer you to show how deep this " +
+                "rabbit hole goes");
+        System.out.println("Choose the Blur Pill and you forget all of these and believe " +
+                "What you want to believe");
+        System.out.println("All Im offering you is the truth");
+        System.out.println("You can do anything you set your Mind Into");
+    }
+}
